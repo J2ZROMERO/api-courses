900354494372ceb7d04c0894fdd95dba6a98ee9e
Luisarfitx7 <luismendozac4m@gmail.com>
2025-07-20 16:28:44 -0600

    add crud users

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     summary="Listar usuarios con paginación",
+     *     security={{"bearerAuth":{}}},
+     *     tags={"Users"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Listado de usuarios paginadas",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Listado de usuarios"),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(property="current_page", type="integer", example=1),
+     *                 @OA\Property(
+     *                     property="data",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="object",
+     *                         @OA\Property(property="id", type="integer", example=1),
+     *                         @OA\Property(property="name", type="string", example="Juan Pérez"),
+     *                         @OA\Property(property="email", type="string", format="email", example="juan@example.com"),
+     *                         @OA\Property(property="created_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z"),
+     *                         @OA\Property(property="updated_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z")
+     *                     )
+     *                 ),
+     *                 @OA\Property(property="first_page_url", type="string", example="http://localhost:8000/api/users?page=1"),
+     *                 @OA\Property(property="from", type="integer", nullable=true, example=null),
+     *                 @OA\Property(property="last_page", type="integer", example=1),
+     *                 @OA\Property(property="last_page_url", type="string", example="http://localhost:8000/api/users?page=1"),
+     *                 @OA\Property(
+     *                     property="links",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="object",
+     *                         @OA\Property(property="url", type="string", nullable=true, example=null),
+     *                         @OA\Property(property="label", type="string", example="&laquo; Previous"),
+     *                         @OA\Property(property="active", type="boolean", example=false)
+     *                     )
+     *                 ),
+     *                 @OA\Property(property="next_page_url", type="string", nullable=true, example=null),
+     *                 @OA\Property(property="path", type="string", example="http://localhost:8000/api/users"),
+     *                 @OA\Property(property="per_page", type="integer", example=15),
+     *                 @OA\Property(property="prev_page_url", type="string", nullable=true, example=null),
+     *                 @OA\Property(property="to", type="integer", nullable=true, example=null),
+     *                 @OA\Property(property="total", type="integer", example=0)
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function index(Request $request)
+    {
+        $users = User::paginate();
+        return response()->json([
+            'message' => 'Listado de usuarios',
+            'data'    => $users,
+        ], 200);
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/users",
+     *     summary="Crear nuevo Usero",
+     *     security={{"bearerAuth":{}}},
+     *     tags={"Users"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name", "password", "email"},
+     *             @OA\Property(property="name", type="string", example="Juan Pérez"),
+     *             @OA\Property(property="email", type="string", format="email", example="juan@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="12345678")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Seccion creado exitosamente",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Juan Pérez"),
+     *             @OA\Property(property="email", type="string", format="email", example="juan@example.com"),
+     *             @OA\Property(property="created_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z"),
+     *             @OA\Property(property="updated_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Errores de validación",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             example={
+     *                 "message": "The given data was invalid.",
+     *                 "errors": {
+     *                     "title": {
+     *                         "The title field is required."
+     *                     },
+     *                     "position": {
+     *                         "The position column must be an integer."
+     *                     },
+     *                      "seccion_id": {
+     *                         "The seccion doesnt exist."
+     *                     }
+     *                 }
+     *             }
+     *         )
+     *     )
+     * )
+     */
+    public function store(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuario registrado',
+            'data'    => $user,
+        ], 201);
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/users/{id}",
+     *     summary="Mostrar un usuario por ID",
+     *     tags={"Users"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Detalles de la sección",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Juan Pérez"),
+     *             @OA\Property(property="email", type="string", format="email", example="juan@example.com"),
+     *             @OA\Property(property="created_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z"),
+     *             @OA\Property(property="updated_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z")
+     *         ),
+     *     ),
+     *     @OA\Response(response=404, description="Usuario no encontrado")
+     * )
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json([
+            'message' => 'Detalles del Usuario',
+            'data'    => $user,
+        ], 200);
+    }
+
+    /**
+     * @OA\Put(
+     *     path="/api/users/{id}",
+     *     summary="Actualizar un usuario",
+     *     tags={"Users"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", example="Juan Pérez"),
+     *             @OA\Property(property="email", type="string", format="email", example="juan@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="12345678")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Seccion actualizado",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer", example=1),
+     *             @OA\Property(property="name", type="string", example="Juan Pérez"),
+     *             @OA\Property(property="email", type="string", format="email", example="juan@example.com"),
+     *             @OA\Property(property="created_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z"),
+     *             @OA\Property(property="updated_at", type="string", format="date-time", example="2025-07-19T15:55:10.000000Z")
+     *         ),
+     *     )
+     * )
+     */
+    public function update(RegisterRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuario Actualizado',
+            'data'    => $user,
+        ], 201);
+    }
+
+    /**
+     * @OA\Delete(
+     *     path="/api/users/{id}",
+     *     summary="Eliminar un Usuario",
+     *     tags={"Users"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=204, description="Usuario eliminado")
+     * )
+     */
+    public function destroy($id)
+    {
+        $User = User::findOrFail($id);
+        $User->delete();
+        return response()->json([
+            'message' => 'Usuario eliminado'
+        ], 204);
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/users/progress",
+     *     summary="Marcar como visto",
+     *     security={{"bearerAuth":{}}},
+     *     tags={"users"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"user_id", "User_id"},
+     *             @OA\Property(property="User_id", type="integer", example=1),
+     *             @OA\Property(property="user_id", type="integer", example=1),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Se ha registrado en un curso"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Errores de validación",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             example={
+     *                 "message": "The given data was invalid.",
+     *                 "errors": {
+     *                     "title": {
+     *                         "The title field is required."
+     *                     },
+     *                     "description": {
+     *                         "The description must be at least 10 characters."
+     *                     },
+     *                      "created_by": {
+     *                         "The user doesnt exist."
+     *                     }
+     *                 }
+     *             }
+     *         )
+     *     )
+     * )
+     */
+    public function progress(ProgressRequest $request)
+    {
+        UserProgress::create($request->all());
+        return response()->json([
+            'message' => 'Se ha guardado el progreso'
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\ElementController;
 use App\Http\Controllers\Api\SectionController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::Post('/login', [AuthController::class, 'login']);
@@ -35,5 +36,13 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/{id}', [ElementController::class, 'update']);
         Route::delete('/{id}', [ElementController::class, 'destroy']);
         Route::post('/progress', [ElementController::class, 'progress']);
+    });
+
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::post('/', [UserController::class, 'store']);
+        Route::get('/{id}', [UserController::class, 'show']);
+        Route::put('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'destroy']);
     });
 });
